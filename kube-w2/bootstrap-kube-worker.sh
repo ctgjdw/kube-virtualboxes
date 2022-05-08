d39bfd5106946092a81756882388a9a5cc128c80
gohchunteck@hotmail.com <gohchunteck@hotmail.com>
2022-05-08 21:52:54 +0800

    update apt install commands

--- kube-w2/bootstrap-kube-worker.sh
+++ kube-w2/bootstrap-kube-worker.sh
@@ -5,17 +5,12 @@ sudo apt-get install ca-certificates software-properties-common apt-transport-ht
 sudo hostnamectl set-hostname "$(hostname -I | awk '{print $2}')"
 sudo swapoff -a && sudo sed -i 's/\/swap/#\/swap/g' /etc/fstab
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list >/dev/null
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
- $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo apt-mark hold docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker.io
+sudo apt-mark hold docker.io
 
 sudo tee /etc/docker/daemon.json >/dev/null <<EOF
 {
